01a516ce756d17225e5afb62dd2a7948d412d959
mchis1908 <mchis19.08@gmail.com>
2024-03-19 11:35:36 +0700

    update

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const ProductRoute = require("./routes/ProductRoute");
+
+const app = express();
+mongoose.connect('mongodb+srv://mchihuynhwork:IFvTIZuaCikqTomR@cluster0.qjz1e7m.mongodb.net/')
+.then(() => {
+  console.log("Connected to database!");
+}).catch(err => {
+  console.log("Connection failed: " + err);
+});
+
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+
+
+app.use("/api/v1/products", ProductRoute);
+
+app.listen(8000, () => {
+  console.log('Server is listening on port 8000');
+});
